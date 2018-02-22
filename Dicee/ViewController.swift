e6b44de7fd431a0e5b9759c3a4c0e2f934950e4f
Javier Jimenez <javierrcc522@gmail.com>
2018-02-22 14:02:22 -0800

    dicee change on click yay

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     
     var randomDiceIndex1 : Int = 0
     var randomDiceIndex2 : Int = 0
+    
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
 
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
@@ -30,6 +32,10 @@ class ViewController: UIViewController {
         //casting the two dicee fron int32 to int
         randomDiceIndex1 = Int(arc4random_uniform(6))
         randomDiceIndex2 = Int(arc4random_uniform(6))
+        
+        //On click change image property
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
         
     }
     
